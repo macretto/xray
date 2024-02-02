01953abbed79420788549f2f94f5ee5f4ef3dbb5
Terrero89 <sergio-terrero@hotmail.com>
2024-02-02 13:09:15 -0500

    added nodemon for hot reload for server

--- api/controllers/exams-controller.js
+++ api/controllers/exams-controller.js
@@ -21,27 +21,22 @@ const getAllExamsController = async (req, res) => {
 
 const getExamController = async (req, res) => {
   const id = req.params.id;
-};
+  res.status(200).json({message: "GETTING SPECIFIC EXAM BY ID --> /api/exams/:id"})
+}
 
 //creates new instance of item
 const createExamController = (req, res) => {
-  res.status(200).json({
-    message: "CREATING SINGLE ITEM FROM /API/EXAMS",
-  });
+  res.status(200).json({message: "CREATING NEW EXAM--> /api/exams"})
 };
 
 //deletes single item by specific id
 const deleteExamController = (req, res) => {
-  res.status(200).json({
-    message: "DELETING SINGLE ITEM FROM /EXAMS/LIST/id",
-  });
+  res.status(200).json({message: "DELETING SPECIFIC EXAM BY ID --> /api/exams/:id"})
 };
 
 //updates single item by specific id
 const updateExamController = (req, res) => {
-  res.status(200).json({
-    message: "UPDATING SINGLE ITEM FROM /EXAMS/LIST/id",
-  });
+  res.status(200).json("UPDATING SPECIFIC EXAM BY ID --> /api/exams/:id")
 };
 
 module.exports = {
