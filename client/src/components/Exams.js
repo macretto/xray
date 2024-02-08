cd09d904006dcb3ff52fcdbd6d8c096c8eea344e
Micheline <MDoughlin@hotmail.com>
2024-02-08 13:05:58 -0500

    Updated Detail Page

--- client/src/components/Exams.js
+++ client/src/components/Exams.js
@@ -3,29 +3,9 @@ import { Button } from "@mui/material";
 
 const Exams = () => {
   return (
-    <div className="exam-container">
-      <div className="image-container">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/React_Logo_SVG.svg/1200px-React_Logo_SVG.svg.png"
-          width={500}
-          height={500}
-        />
-      </div>
-      <div className="text-container">
-        <h1>Patient Info</h1>
-        <p>Patient ID: </p>
-        <p>Age: </p>
-        <p>Sex: </p>
-        <p>BMI: </p>
-        <p>Zip Code: </p>
-        <p>Exam ID: </p>
-        <p>Date</p>
-        <Button variant="outlined">Edit</Button>
-        <Button variant="outlined" color="error">
-          Delete
-        </Button>
-      </div>
-    </div>
+    <>
+      <h1> Exam page</h1>
+    </>
   );
 };
 
