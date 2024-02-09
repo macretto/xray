8d8619389a3e2923430910a30a4d74c399124bb8
Mac Retto <mac.retto@gmail.com>
2024-02-09 10:54:50 -0500

    Nav bar (#6)
    * Completed Nav Bar and Home Page
    
    * Visual Search Bar
    
    * Details Page Outline
    
    * Visual Details Page
    
    * Updated Detail Page
    
    * Fields in Detail Page
    
    ---------
    
    Signed-off-by: Mac Retto <mac.retto@gmail.com>
    Co-authored-by: Micheline <MDoughlin@hotmail.com>

---
+++ client/src/components/DetailPage.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Button, TextField } from "@mui/material";
+
+
+
+const DetailPage = () => {
+  return (
+    <div className="exam-container">
+      <div className="image-container">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/React_Logo_SVG.svg/1200px-React_Logo_SVG.svg.png"
+          width={500}
+          height={500}
+        />
+      </div>
+      <div className="text-container">
+        <h1>Patient Info</h1>
+        <p>Patient ID: </p>
+        <p>Age: </p>
+        <p>Sex: </p>
+        <p>BMI: </p>
+        <p>Zip Code: </p>
+        <p>Exam ID: </p>
+        <p>Date</p>
+        <Button variant="outlined">Edit</Button>
+        <Button variant="outlined" color="error">
+          Delete
+        </Button>
+      </div>
+    </div>
+  );
+};
+export default DetailPage;
---
+++ client/src/components/Exams.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@mui/material";
+
+const Exams = () => {
+  return (
+    <>
+      <h1> Exam page</h1>
+    </>
+  );
+};
+
+export default Exams;
