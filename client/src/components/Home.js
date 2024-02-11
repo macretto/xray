0bd0c9b09860ee29f167d6ead641f7c82ccd6b94
Micheline <MDoughlin@hotmail.com>
2024-02-11 16:08:18 -0500

    Image showing and code cleaned up

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -10,10 +10,7 @@ import Paper from '@mui/material/Paper';
 import { Link } from 'react-router-dom';
 
 
-
 export default function DenseTable() {
-
-
 
   const [data, setData] = useState([]);
 
@@ -26,37 +23,20 @@ export default function DenseTable() {
     setData(await response.json());
   };
 
-  function createData(patientId, examId, image, keyFindings, brixiaScores, age, sex, bmi, zipCode) {
-    return { patientId, examId, image, keyFindings, brixiaScores, age, sex, bmi, zipCode };
+  function createData(patientId, examId, imageURL, keyFindings, brixiaScores, age, sex, bmi, zipCode) {
+    return { patientId, examId, imageURL, keyFindings, brixiaScores, age, sex, bmi, zipCode };
   }
-
-  const rows = [
-    createData(98056, 78764, 'image', 'broken', 15, 25, 'M', 33, 123),
-    // hard code
-  ];
-
-  console.log(data);
-
-
-  // const ExamData = () => {
-  //   const [exams, setExams] = useState([]);
-
-  //   React.useEffect(() => {
-  //     fetch(`http://localhost:9000/api/exams`);
-
-  //   });
-  // };
 
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
         <TableHead>
           <TableRow>
-            <TableCell align="right">Patient ID</TableCell>
+            <TableCell align="center">Patient ID</TableCell>
             <TableCell align="right">Exam ID </TableCell>
             <TableCell align="right">Image</TableCell>
-            <TableCell align="right">Key Findings</TableCell>
-            <TableCell align="right">Brixia Score</TableCell>
+            <TableCell align="center">Key Findings</TableCell>
+            <TableCell align="center">Brixia Score</TableCell>
             <TableCell align="right">Age</TableCell>
             <TableCell align="right">Sex</TableCell>
             <TableCell align="right">BMI</TableCell>
@@ -71,15 +51,13 @@ export default function DenseTable() {
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               {/* Change this */}
-              <TableCell align="right" component="th" scope="row">
-                {data.patientId}
-              </TableCell>
+              <TableCell align="center">{data.patientId}</TableCell>
               <Link to="/">
                 <TableCell align="right">{data.examId}</TableCell>
               </Link>
-              <TableCell align="right">{data.image}</TableCell>
-              <TableCell align="right">{data.keyFindings}</TableCell>
-              <TableCell align="right">{data.brixiaScores}</TableCell>
+              <TableCell align="right"> <img src={data.imageURL} style={{ width: '50px', height: '50px' }} /></TableCell>
+              <TableCell align="center">{data.keyFindings}</TableCell>
+              <TableCell align="center">{data.brixiaScores}</TableCell>
               <TableCell align="right">{data.age}</TableCell>
               <TableCell align="right">{data.sex}</TableCell>
               <TableCell align="right">{data.bmi}</TableCell>
