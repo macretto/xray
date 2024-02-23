8ed379e5629b41f11a4cde4eed68bbb02e9b44c0
Micheline <MDoughlin@hotmail.com>
2024-02-22 23:18:57 -0500

    Complete Exam List Compnent

--- client/src/components/ExamItem.js
+++ client/src/components/ExamItem.js
@@ -2,20 +2,20 @@
 
 
 
-const ExamItem = ({item})=>{
+const ExamItem = ({ item }) => {
 
-    return (
-       <>
-        <li>{item._id}</li>
-        <li>{item.patientId}</li>
-        <img src={item._imageURL}/>
-        <li>{item.patientId}</li>
-        <li>{item._id}</li>
-        <li>{item.patientId}</li>
-       
-       </>
-     
-      );
-    };
+  return (
+    <>
+      <li>{item._id}</li>
+      <li>{item.patientId}</li>
+      <img src={item._imageURL} />
+      <li>{item.patientId}</li>
+      <li>{item._id}</li>
+      <li>{item.patientId}</li>
+
+    </>
+
+  );
+};
 
 export default ExamItem;
--- client/src/components/ExamsList.js
+++ client/src/components/ExamsList.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import ExamItem from "./ExamItem";
 
-const ExamsList = ({items}) => {
-  
-    return (
-        <ul>
+const ExamsList = ({ items }) => {
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th> Patient ID</th>
+            <th> Exam ID</th>
+            <th> Image</th>
+            <th> Key Findings</th>
+            <th> Brixia Score</th>
+            <th> Age</th>
+            <th> Sex</th>
+            <th> BMI</th>
+            <th> Zip Code</th>
+          </tr>
+        </thead>
+
+        <tbody>
           {items.map(item => (
-            <ExamItem key={item._id} item={item} />
+            <tr key={item._id}>
+              <td>{item.patientId}</td>
+              <td>{item.examId}</td>
+              <td><img src={item.imageURL} alt={`Patient ${item.patientId}`} width="50" height="50" /></td>
+              <td>{item.keyFindings}</td>
+              <td>{item.brixiaScores}</td>
+              <td>{item.age}</td>
+              <td>{item.sex}</td>
+              <td>{item.bmi}</td>
+              <td>{item.zipCode}</td>
+            </tr>
           ))}
-        </ul>
-      );
-          }
+        </tbody>
+      </table>
+    </div>
+  );
+};
 export default ExamsList;
